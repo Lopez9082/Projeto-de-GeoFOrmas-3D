bc94580f6340f6f7e34acec66627f25e877ee8c3
Lopez9082 <joseauguxto6@gmail.com>
2025-11-24 13:45:12 -0300

    imagem atualizada somente

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -248,7 +248,7 @@
         </div>
     </div>
 
-    <img src="https://images.unsplash.com/photo-1634034379073-f689b460a3fc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8JUMzJThEY29uZXMlMjB2ZXRvcmlhaXN8ZW58MHx8MHx8fDA%3D" alt="Math Image">
+    <img src="https://img.freepik.com/vetores-premium/icone-de-simbolos-matematicos_1186366-125286.jpg" alt="Math Image">
 </section>
 
 <!-- SOBRE -->
